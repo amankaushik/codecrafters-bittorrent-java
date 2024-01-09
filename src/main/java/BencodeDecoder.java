24782ff0f3c9580a55c1c61f5eeeb124298cfca0
Aman Kaushik <amankaushik104@gmail.com>
2024-01-08 19:41:13 -0800

    implement decoding dictionaries

--- src/main/java/BencodeDecoder.java
+++ src/main/java/BencodeDecoder.java
@@ -1,20 +1,15 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+// import java.util.TreeMap;
 import java.util.List;
-import java.util.Stack;
+import java.util.Map;
+
+// import com.google.gson.Gson;
 
 public class BencodeDecoder {
     static List<Object> result = new ArrayList<>();
-
-    static int findClosingIndex(String input, int startIndex) {
-        var ch = input.charAt(startIndex);
-        var closingIndex = startIndex;
-        while (true && ch != 'e') {
-            closingIndex++;
-            ch = input.charAt(closingIndex);
-        }
-        return closingIndex;
-    }
+    // private static final Gson gson = new Gson();
 
     static DecoderResult _decode(String input) {
         // System.out.println("Input: " + input);
@@ -28,6 +23,18 @@ public class BencodeDecoder {
         int i = 0;
 
         for (; i < totalLength; i++) {
+            if (input.charAt(i) == 'd') {
+                var op = _decode(input.substring(i + 1));
+                // System.out.println(op.result);
+                Map<Object, Object> resMap = new HashMap();
+                for (int k = 0; k < op.result.size(); k += 2) {
+                    resMap.put(op.result.get(k), op.result.get(k + 1));
+                }
+                // System.out.print(gson.toJson(resMap).replaceAll("\\\\", "").replace("\"\"", "\""));
+                // System.out.println(resMap.toString().replaceAll("=", ":").replaceAll(" ", ""));
+                result.add(resMap.toString().replaceAll("=", ":").replaceAll(" ", ""));
+                i = op.nextIndex;
+            }
             if (input.charAt(i) == 'l') {
                 var op = _decode(input.substring(i + 1));
                 result.add(op.result);
@@ -78,98 +85,5 @@ public class BencodeDecoder {
             }
         }
         return new DecoderResult(result, i + 1);
-    }
-
-    static List<Object> _decodeList(String input) {
-
-        return Collections.emptyList();
-    }
-
-    static List<Object> decode(String input) {
-        // System.out.println("Input: " + input);
-        if (input.length() < 3) {
-            return Collections.emptyList();
-        }
-        var result = new ArrayList<Object>();
-        var totalLength = input.length();
-
-        for (int i = 0; i < totalLength; i++) {
-            if (input.charAt(i) == 'l') {
-                // System.out.println("i: " + i);
-                var closingIndex = findListClosingIndex(input, i);
-                // System.out.println("e: " + closingIndex);
-                // System.out.println("Encountered 'l', skipping iteration");
-                // var closingIndex = totalLength-1;
-                // var ch = input.charAt(closingIndex);
-
-                // while (true && ch != 'e') {
-                    // closingIndex--;
-                // }
-                result.add(decode(input.substring(i + 1, closingIndex)));
-                i = closingIndex;
-            } else if (input.charAt(i) == 'i') {
-                var ch = input.charAt(i);
-                var workingInput = new StringBuilder();
-
-                while (true && ch != 'e') {
-                    workingInput.append(ch);
-                    i++;
-                    ch = input.charAt(i);
-                }
-
-                workingInput.append(ch);
-                // System.out.println(workingInput);
-                result.add(IntegerDecoder.decode(workingInput.toString()));
-                // System.out.println(result);
-
-            } else if (Character.isDigit(input.charAt(i))) {
-                // System.out.println(i);;
-                var expectedLengthString = new StringBuilder();
-                var ch = input.charAt(i);
-                while (ch != ':') {
-                    expectedLengthString.append(ch);
-                    i++;
-                    ch = input.charAt(i);
-                }
-                
-                var expectedLength = Integer.parseInt(expectedLengthString.toString());
-                var colonChar = input.charAt(i);
-
-                if (colonChar != ':') {
-                    throw new RuntimeException("Expected the char to be ':'");
-                }
-
-                if (i + expectedLength > input.length()) {
-                    throw new RuntimeException("Working input cannot be greater than provided input");
-                }
-
-                i++;
-                var workingInput = input.substring(i, i + expectedLength);
-                i = i + expectedLength - 1;
-                result.add(StringDecoder.decode(workingInput));
-            } else {
-                throw new RuntimeException("Unexpected character at the beginning of the input");
-            }
-        }
-        return result;
-    }
-
-    private static int findListClosingIndex(String input, int startIndex) {
-        var ch = input.charAt(startIndex);
-        var closingIndex = startIndex;
-        var stack = new Stack<Character>();
-        while (true) {
-            if (ch == 'l' || ch == 'i') {
-                stack.push(ch);
-            } else if (ch == 'e') {
-                stack.pop();
-                if (stack.isEmpty()) {
-                    break;
-                }
-            }
-            closingIndex++;
-            ch = input.charAt(closingIndex);
-        }
-        return closingIndex;
     }
 }
